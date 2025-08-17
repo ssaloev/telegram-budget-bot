9225518db4288f73a1afeb3f5b08f8547fdcbc26
sadi_saloev <sadi_saloev@elon.team>
2025-08-18 01:23:29 +0500

    fix: connect to DB

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,6 @@ export async function main() {
     const mongoUri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/telegramBotBudget";
 
     await mongoose.connect(mongoUri, {
-        authSource: "admin" // needed if MongoDB has auth enabled
+        authSource: "admin"
     });
 }
